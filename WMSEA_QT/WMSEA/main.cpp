23fb28344f26aa3d688e0b6af52db11403cac902
Houle Adrien <adrienhoule01@gmail.com>
2023-11-13 09:37:11 +0100

    modif main

--- WMSEA_QT/WMSEA/main.cpp
+++ WMSEA_QT/WMSEA/main.cpp
@@ -2,7 +2,7 @@
 
 int main(int argc, char *argv[])
 {
-    KMeansAnalyzer kma = KMeansAnalyzer("../../datasets/truemarblepartials/test2");
+    KMeansAnalyzer kma = KMeansAnalyzer("../../datasets/truemarblepartials/");
     kma.performKMeansClustering(10);
     kma.generateClusteredImages();
     return 0;
